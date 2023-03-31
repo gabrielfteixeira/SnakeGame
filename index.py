6803e592b62cc409ac1748e295a21506382084c1
Gabriel Teixeira <gabriel.teixeira009@gmail.com>
2023-03-31 13:33:59 -0300

    Update index.py
    jogo da cobrinha já funcionando e jogavel

--- index.py
+++ index.py
@@ -17,6 +17,12 @@ altura = 480
 x_cobra = int(largura/2)
 y_cobra = int(altura/2)
 
+velocidade = 10
+x_controle = velocidade
+y_controle = 0
+
+
+
 x_maca = randint(40,600)
 y_maca = randint(50,430)
 
@@ -28,29 +34,66 @@ pygame.display.set_caption('jogo Dos Quadrados')
 relogio = pygame.time.Clock()
 
 lista_cobra = []
+comprimento_inicial = 5
+morreu = False
 
 def aumenta_cobra(lista_cobra):
     for XeY in lista_cobra:
         pygame.draw.rect(tela,(0,255,0),(XeY[0],XeY[1],20,20))
+
+def reiniciar_jogo():
+    global pontos, comprimento_inicial, x_cobra, y_cobra,lista_cobra, lista_cabeca, x_maca, y_maca, morreu
+    pontos = 0
+    comprimento_inicial = 5
+    x_cobra = int(largura/2)
+    y_cobra = int(altura/2)
+    lista_cobra = []
+    lista_cabeca = []
+    x_maca = randint(40,600)
+    y_maca = randint(50,430)
+    morreu = False
+
+
 
 while True:
     relogio.tick(30)
     tela.fill((255,255,255))
     mensagem = f'Pontos: {pontos}'
     texto_formatado = fonte.render(mensagem, True, (0,0,0))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        
-        if pygame.key.get_pressed()[K_a]:
-            x_cobra = x_cobra-20
-        if pygame.key.get_pressed()[K_d]:
-            x_cobra = x_cobra + 20
-        if pygame.key.get_pressed()[K_w]:
-            y_cobra = y_cobra - 20
-        if pygame.key.get_pressed()[K_s]:
-            y_cobra = y_cobra + 20
+
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle  = -velocidade
+                    y_controle = 0
+            if event.key == K_d:
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle  = velocidade
+                    y_controle = 0
+            if event.key == K_w:
+                if y_controle == velocidade:
+                    pass
+                else:
+                    x_controle  = 0
+                    y_controle = -velocidade
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    x_controle  = 0
+                    y_controle = velocidade
+
+    x_cobra = x_cobra +x_controle
+    y_cobra = y_cobra + y_controle
 
     cobra = pygame.draw.rect(tela,(0,255,0),(x_cobra,y_cobra,20,20))
     maca = pygame.draw.rect(tela,(255,0,0),(x_maca,y_maca,20,20))
@@ -60,12 +103,45 @@ while True:
         y_maca = randint(50,430)
         pontos = pontos + 1
         barulho_colisao.play()
+        comprimento_inicial = comprimento_inicial + 1
 
     lista_cabeca = []
     lista_cabeca.append(x_cobra)
     lista_cabeca.append(y_cobra)
     
     lista_cobra.append(lista_cabeca)
+
+    if lista_cobra.count(lista_cabeca) > 1:
+        fonte2 = pygame.font.SysFont('arial',20,True, True)
+        mensagem = 'Game Over! Precione a tecla R para jogar novamente'
+        texto_formatado = fonte2.render(mensagem, True, (0,0,0))
+        ret_texto = texto_formatado.get_rect();
+
+        morreu = True
+        while morreu:
+            tela.fill((255,255,255))
+            for event in pygame.event.get():
+                if event.type ==QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        reiniciar_jogo()
+            ret_texto.center = (largura//2, altura//2)
+            tela.blit(texto_formatado,ret_texto)
+            pygame.display.update()
+
+    if x_cobra > largura:
+        x_cobra = 0
+    if x_cobra < 0:
+        x_cobra = largura
+    if y_cobra < 0:
+        y_cobra = altura
+    if y_cobra > altura:
+        y_cobra = 0 
+
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
 
     aumenta_cobra(lista_cobra)
 
